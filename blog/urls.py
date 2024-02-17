657e5d9339bf90d5b17e48bcfcb0d4bbcd2444b9
Joha Wilson <johawilsonatu@icloud.com>
2024-02-17 19:42:09 +0000

    created blog detail views, urls and template

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('post_blog/', views.post_blog, name='post_blog'),
     path("blog_list", views.BlogList.as_view(), name="blog_list"),
+    path("<slug:slug>/", views.BlogDetail.as_view(), name="blog_detail"),
 ]
