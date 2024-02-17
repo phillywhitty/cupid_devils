98bf7f9d52beb642dfbc646c82fdf5dd4cfb5341
TeodoraAlina <alina.teower@gmail.com>
2024-02-17 13:54:57 +0000

    Build: admin site, BlogList view and urls

--- blog/models.py
+++ blog/models.py
@@ -2,19 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-
+STATUS = ((0, "Draft"), (1, "Published"))
 
 class Blog(models.Model):
 
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
     related_name="blog_post", null=True)
     updated_on = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
+    status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name="blog_likes", blank=True)
     excerpt = models.TextField(blank=True)
 
