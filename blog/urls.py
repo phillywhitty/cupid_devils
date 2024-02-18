09565adb365653a84a4338f47136142bd3f29e93
Joha Wilson <johawilsonatu@icloud.com>
2024-02-18 05:25:06 +0000

    created post comment views, urls and adjusted BlogForm fields

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('post_blog/', views.post_blog, name='post_blog'),
+    path('<int:blog_id>/', views.post_comment, name='post_comment'),
     path("blog_list/", views.BlogList.as_view(), name="blog_list"),
     path("<int:blog_id>/", views.blog_details, name="blog_details"),
     path('like/<int:blog_id>/', views.BlogLike.as_view(), name='blog_like'),
